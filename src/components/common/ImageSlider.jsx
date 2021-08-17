3c3324553bc35d89741a3c19c4a409944b723065
Tyler Marshall <tylerjeremiahh@gmail.com>
2021-08-17 21:26:52 +0100

    Pulling through the images from the product passed through to the image slider component

--- src/components/common/ImageSlider.jsx
+++ src/components/common/ImageSlider.jsx
@@ -1,35 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 import productOneAsArray from 'assets/images/products/replenish';
+import images from 'assets/images/products';
 import './styles/ImageSlider';
 
 export default function ImageSlider({product}) {
-
     const [imageIndex, setImageIndex] = useState(0);
     const [imageModalState, setImageModalState] = useState(false);
+    const [imageCollection, setImageCollection] = useState([]);
 
-    const changeImage = image => {
-        const imageIndex = productOneAsArray.findIndex(img => img === image);
-        setImageIndex(imageIndex);
+    useEffect(() => {
+        setImageCollection(images[product.MediaCollection]);
+    }, []);
+
+    const changeImage = index => {
+        setImageIndex(index);
     };
 
     useInterval(() => {
-        const imageIdx = imageIndex === productOneAsArray.length - 1 ? 0 : imageIndex + 1;
+        const imageIdx = imageIndex === imageCollection.length - 1 ? 0 : imageIndex + 1;
         setImageIndex(imageIdx);
     }, imageModalState ? null : 6000); //Stop slider when image is enlarged
 
     return (
         <div className="image-slider">
             <Modal centered={true} dialogClassName="image-modal-dialog" show={imageModalState} onHide={() => setImageModalState(false)}>
-                <img className="modal-image" src={productOneAsArray[imageIndex]} alt="Large Product Image" />
+                <img className="modal-image" src={imageCollection[imageIndex]} alt="Large Product Image" />
             </Modal>
             <div className="main-image-container">
-                <img src={productOneAsArray[imageIndex]} onClick={() => setImageModalState(true)} alt="Product Image" className="main-image" />
+                <img src={imageCollection[imageIndex]} onClick={() => setImageModalState(true)} alt="Product Image" className="main-image" />
             </div>
             <div className="thumbnail-container">
-                {productOneAsArray.map((image, imageIdx) => {
+                {imageCollection.map((image, imageIdx) => {
                     return (
-                        <div className="thumbnail" key={imageIdx} onClick={() => changeImage(image)}>
+                        <div className="thumbnail" key={imageIdx} onClick={() => changeImage(imageIdx)}>
                             <img src={image} alt="Product thumbnail"
                                 className={`thumbnail-image ${imageIndex === imageIdx ? "active" : ""}`} />
                         </div>
