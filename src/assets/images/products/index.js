7b69a2040ab7d6cf6614ae7c7a16d2d6474768c7
Tyler Marshall <tylerjeremiahh@gmail.com>
2021-08-22 19:01:02 +0100

    Adding note

--- src/assets/images/products/index.js
+++ src/assets/images/products/index.js
@@ -4,4 +4,8 @@ import hydrate from './hydrate';
 import nourish from './nourish';
 import replenish from './replenish';
 
+/**
+ * Note:    These are large, unoptimised images. These will need optimising before this project
+ *          is ever deployed.
+ */
 export default {elixir, enhance, hydrate, nourish, replenish};
